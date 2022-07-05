cc800267f118fc81dd80d41bd57c46ea78559713
MOSergi <sergioorozmendez@gmail.com>
2022-07-05 17:26:37 +0200

    arreglado bug al mostrar el menu desplegable

--- test-app-youtube-frontend-clone-withclass/src/components/Menu.jsx
+++ test-app-youtube-frontend-clone-withclass/src/components/Menu.jsx
@@ -21,25 +21,28 @@ export default class Menu extends React.Component {
 
     render(){
         return(
-            <nav className="Menu">
-                <div className="firstSubMenu">
-                    {this.state.dropDown ? <DropDownMenu /> : <></>}
-                    <h3 onClick={()=>{this.state.dropDown ? this.setState({dropDown : false}) : this.setState({dropDown : true})}}><FaBars /></h3>
-                    <h2><Link className="links" to='/'><FaYoutube style={{color : "#FF0000"}}/>Youtube</Link></h2>
-                </div>
-                <div className="buscador">
-                    <input type="text" placeholder="Buscar"/>
-                    <button className="buscar"><FaSearch /></button>
-                    <button className="microfono"><FaMicrophone /></button>
-                </div>  
-                <div className="LoginAndConfigs">
-                    <h3><FaTh /></h3>
-                    <h3><FaEllipsisV /></h3>
-                    <div className="loginMenu">
-                        <h3 style={{color: "#1468D6"}}><FaUserAlt style={{color : "rgb(33, 142, 214)", marginRight : "5px"}}/> Iniciar Sesión</h3>
+            <>                    
+                {this.state.dropDown ? <DropDownMenu /> : <></>}
+                <nav className="Menu">
+                    <div className="firstSubMenu">
+                        <h3 onClick={()=>{this.state.dropDown ? this.setState({dropDown : false}) : this.setState({dropDown : true})}}><FaBars /></h3>
+                        <h2><Link className="links" to='/'><FaYoutube style={{color : "#FF0000"}}/>Youtube</Link></h2>
                     </div>
-                </div>
-            </nav>
+                    <div className="buscador">
+                        <input type="text" placeholder="Buscar"/>
+                        <button className="buscar"><FaSearch /></button>
+                        <button className="microfono"><FaMicrophone /></button>
+                    </div>  
+                    <div className="LoginAndConfigs">
+                        <h3><FaTh /></h3>
+                        <h3><FaEllipsisV /></h3>
+                        <div className="loginMenu">
+                            <h3 style={{color: "#1468D6"}}><FaUserAlt style={{color : "rgb(33, 142, 214)", marginRight : "5px"}}/> Iniciar Sesión</h3>
+                        </div>
+                    </div>
+                </nav>
+            </>
+            
         );
     }
 
