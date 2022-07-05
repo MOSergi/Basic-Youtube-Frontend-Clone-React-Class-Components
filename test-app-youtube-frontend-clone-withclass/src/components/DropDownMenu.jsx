4edacc1cfda494c3bfd09a0b906bd127eb5bb8a3
MOSergi <sergioorozmendez@gmail.com>
2022-07-05 18:36:09 +0200

    Menu casi acabado

--- test-app-youtube-frontend-clone-withclass/src/components/DropDownMenu.jsx
+++ test-app-youtube-frontend-clone-withclass/src/components/DropDownMenu.jsx
@@ -3,17 +3,20 @@ import React from "react";
 import "../styles/DropDownMenu.css";
 //react router dom
 import { NavLink } from "react-router-dom";
+//icons 
+import { FaHome, FaRegCompass, FaFileVideo, FaYoutubeSquare, FaHistory, FaBook } from "react-icons/fa";
 
 export default class DropDownMenu extends React.Component {
     render(){
         return(
             <nav className="dropdownMenu">
                 <ul className="dropdown">
-                    <NavLink style={({isActive})=>{return isActive ? {backgroundColor : "#E5E5E5"} : undefined}} className="linksDrop" to='/'>Inicio</NavLink>
-                    <NavLink style={({isActive})=>{return isActive ? {backgroundColor : "#E5E5E5"} : undefined}} className="linksDrop" to='/Explorer'>Explorar</NavLink>
-                    <NavLink style={({isActive})=>{return isActive ? {backgroundColor : "#E5E5E5"} : undefined}} className="linksDrop" to='/Shorts'>Shorts</NavLink>
-                    <NavLink style={({isActive})=>{return isActive ? {backgroundColor : "#E5E5E5"} : undefined}} className="linksDrop" to='/Subscriptions'>Suscripciones</NavLink>
-                    <NavLink style={({isActive})=>{return isActive ? {backgroundColor : "#E5E5E5"} : undefined}} className="linksDrop" to='/Historial'>Historial</NavLink>
+                    <NavLink style={({isActive})=>{return isActive ? {backgroundColor : "#E5E5E5"} : undefined}} className="linksDrop" to='/'><FaHome /> Inicio</NavLink>
+                    <NavLink style={({isActive})=>{return isActive ? {backgroundColor : "#E5E5E5"} : undefined}} className="linksDrop" to='/Explorer'><FaRegCompass /> Explorar</NavLink>
+                    <NavLink style={({isActive})=>{return isActive ? {backgroundColor : "#E5E5E5"} : undefined}} className="linksDrop" to='/Shorts'><FaFileVideo /> Shorts</NavLink>
+                    <NavLink style={({isActive})=>{return isActive ? {backgroundColor : "#E5E5E5"} : undefined}} className="linksDrop" to='/Subscriptions'><FaYoutubeSquare /> Suscripciones</NavLink>
+                    <NavLink style={({isActive})=>{return isActive ? {backgroundColor : "#E5E5E5"} : undefined}} className="linksDrop" to='/Biblioteca'><FaBook /> Biblioteca</NavLink>
+                    <NavLink style={({isActive})=>{return isActive ? {backgroundColor : "#E5E5E5"} : undefined}} className="linksDrop" to='/Historial'><FaHistory /> Historial</NavLink>
                 </ul>
             </nav>
         );
